2f1591d153d20fa3174946445b5bfe236fae6322
RuaaNoman <roaanoman@gmail.com>
2022-07-13 00:00:10 +0300

    routing section done

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/hello', function(){
+//     return response('<h1>Not Found</h1>', 404)
+//     -> header('Content-Type', 'text/plain');
+// });
+
+// // post.php?id=13
+// Route::get('/post/{id}', function($id){
+//     ddd($id);
+//      return response('post' . $id);
+//     //  add condition to only take numbers as id
+//     // where('name', regex)
+//     // name must be == to what's in the route name
+// }) -> where('id', '[0-9]+');
+
+// // what is the diff bt this and the one above?
+// Route::get('/search', function(Request $request){
+//     return $request-> name . ' ' . $request->city;
+// });
+
+
+// die & dump
+// dd() shows whatever passed to it and terminate the code
+// die, dump & debug
+// ddd() full debug
