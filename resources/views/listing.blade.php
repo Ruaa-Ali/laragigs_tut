eb600a80b603dbd41d6dfaa77293b1957241faf4
RuaaNoman <roaanoman@gmail.com>
2022-07-15 07:06:12 +0300

    half part of templates and UI done

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,2 +1,7 @@
-<h2>{{$listing['title']}}</h2>
-<p>{{$listing['description']}}</p>
+@extends('layout')
+
+@section('content')
+@include('partials/_search')
+    <h2>{{$listing['title']}}</h2>
+    <p>{{$listing['description']}}</p>
+@endsection
